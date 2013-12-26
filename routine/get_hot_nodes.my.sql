06fc993ffb3e3229f8bb8809c4fd55384a2fb4c7
Alex Tian <alex.tian@geekpush.com>
2013-12-26 06:50:59 +0000

    created some stored procedures

---
+++ routine/get_hot_nodes.my.sql
@@ -0,0 +1,20 @@
+DROP PROCEDURE IF EXISTS get_hot_nodes;
+
+DELIMITER ;;
+CREATE DEFINER=web@localhost PROCEDURE get_hot_nodes(IN $tids VARCHAR(255), IN $time INT, IN $limit INT)
+    COMMENT 'get recent hot nodes for a list of given tids'  
+BEGIN
+	IF LOCATE(',', $tids) = 0 THEN
+		SELECT id, title, (SELECT COUNT(*) FROM comments AS c WHERE c.nid = n.id) AS comment_count 
+		FROM nodes AS n 
+		WHERE create_time > $time AND tid = CAST($tids AS UNSIGNED INTEGER) AND status = 1 
+		ORDER BY comment_count DESC 
+		LIMIT $limit;
+	ELSE
+		SET @sql = CONCAT('SELECT id, title, (SELECT COUNT(*) FROM comments AS c WHERE c.nid = n.id) AS comment_count FROM nodes AS n WHERE create_time > ', $time, ' AND tid IN (', $tids, ') AND status = 1 ORDER BY comment_count DESC LIMIT ', $limit);
+		PREPARE stmt FROM @sql;
+		EXECUTE stmt;
+		DEALLOCATE PREPARE stmt;
+	END IF;
+END ;;
+DELIMITER ;
