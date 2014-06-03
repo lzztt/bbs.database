539923a118e201d2197c2dd2c71eea232c8673dd
Long <15257546+lzztt@users.noreply.github.com>
2014-06-03 02:13:00 +0000

    update changes for wedding and new controller

--- routine/get_pm.my.sql
+++ routine/get_pm.my.sql
@@ -3,7 +3,15 @@ DROP PROCEDURE IF EXISTS get_pm;
 DELIMITER ;;
 CREATE DEFINER=web@localhost PROCEDURE get_pm(IN $mid INT, IN $uid INT)
     COMMENT 'get pm count in inbox for a given user'  
-BEGIN
+sp: BEGIN
+	DECLARE $found INT DEFAULT 0;
+
+	SELECT IF(from_uid = $uid, 1, IF(to_uid = $uid, 1, 0)) INTO $found FROM priv_msgs WHERE msg_id = $mid LIMIT 1;
+	IF $found != 1 THEN
+		SELECT NULL LIMIT 0;
+		LEAVE sp;
+	END IF;
+
     UPDATE priv_msgs SET to_status = 1 WHERE msg_id = $mid AND to_uid = $uid AND to_status = 2;
 
     SELECT m.id, m.time, m.body, u.id AS uid, u.username, u.avatar 
