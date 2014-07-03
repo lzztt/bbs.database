d42abed77b3dbfc46b59a65be93b44e29ea50e7f
Long <15257546+lzztt@users.noreply.github.com>
2014-07-03 04:35:45 +0000

    update changes

--- routine/get_pm_list_inbox.my.sql
+++ routine/get_pm_list_inbox.my.sql
@@ -4,12 +4,11 @@ DELIMITER ;;
 CREATE DEFINER=web@localhost PROCEDURE get_pm_list_inbox(IN $uid INT, IN $limit INT, IN $offset INT)
     COMMENT 'get recent private messages in inbox for a given user'  
 BEGIN
-    SELECT m.id, m.from_uid, m.to_uid, m.body, m.time,
-        (SELECT username FROM users WHERE id = m.from_uid) AS from_name, 
-        (SELECT username FROM users WHERE id = m.to_uid) AS to_name, 
-        g.pm_time, g.has_new AS is_new
+    SELECT g.msg_id, m.body, g.from_uid AS uid,
+        (SELECT username FROM users WHERE id = g.from_uid) AS user, 
+        g.pm_time AS time, g.has_new AS is_new
     FROM priv_msgs AS m
-        JOIN (SELECT msg_id, MAX(time) AS pm_time, (SUM(to_status) > COUNT(to_status)) AS has_new FROM priv_msgs WHERE to_uid = $uid AND to_status > 0 GROUP BY msg_id ORDER BY pm_time DESC LIMIT $limit OFFSET $offset) AS g
+        JOIN (SELECT msg_id, MAX(time) AS pm_time, from_uid, (SUM(to_status) > COUNT(to_status)) AS has_new FROM priv_msgs WHERE to_uid = $uid AND to_status > 0 GROUP BY msg_id ORDER BY pm_time DESC LIMIT $limit OFFSET $offset) AS g
         ON m.id = g.msg_id
     ORDER BY g.pm_time DESC;
 END ;;
