fc3ff42ad88ad79f187978c77b1cd14e82709800
Long <15257546+lzztt@users.noreply.github.com>
2014-07-22 02:20:09 +0000

    new single site

--- routine/get_age_stat_single.my.sql
+++ routine/get_age_stat_single.my.sql
@@ -1,10 +1,9 @@
 DROP PROCEDURE IF EXISTS get_age_stat_single;
 
 DELIMITER ;;
-CREATE DEFINER=web@localhost PROCEDURE get_age_stat_single(IN $begin INT, IN $end INT)
+CREATE DEFINER=web@localhost PROCEDURE get_age_stat_single(IN $aid INT)
     COMMENT 'get attendee count'  
 BEGIN
-    SELECT sex, age, COUNT(id) AS count FROM ff_attendees WHERE time > $begin AND time < $end GROUP BY sex, age;
-    SELECT COUNT(NULLIF(sex, 0)) AS male, COUNT(NULLIF(sex, 1)) AS female FROM ff_attendees WHERE time > $time;
+    SELECT sex, age, COUNT(id) AS count FROM ff_attendees WHERE aid = $aid GROUP BY sex, age;
 END ;;
 DELIMITER ;
