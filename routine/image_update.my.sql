670d8219bccd987e923bd0b3f70238acc71eb495
Long <15257546+lzztt@users.noreply.github.com>
2014-08-23 21:11:08 +0000

    working new version

---
+++ routine/image_update.my.sql
@@ -0,0 +1,9 @@
+DROP PROCEDURE IF EXISTS image_update;
+
+DELIMITER ;;
+CREATE DEFINER=web@localhost PROCEDURE image_update(IN $id INT, IN $name VARCHAR(60))
+    COMMENT 'update an image'  
+BEGIN
+    UPDATE images SET name = $name WHERE id = $id;
+END ;;
+DELIMITER ;
