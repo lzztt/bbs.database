7162ce3cc0934a61b201dd734086fb085cfda05e
Alex Tian <alex.tian@geekpush.com>
2014-09-19 03:52:12 +0000

    change forum page title

--- routine/get_recent_images.my.sql
+++ routine/get_recent_images.my.sql
@@ -25,7 +25,7 @@ BEGIN
 		FROM (
 			SELECT i.nid, i.name, i.path, n.title, @rn := IF( @nid = i.nid, @rn := @rn + 1, 1 ) AS rn, @nid := i.nid
 			FROM images AS i JOIN nodes AS n ON i.nid = n.id 
-			WHERE (n.tid = 139 AND n.status = 1) AND i.city_id = $city_id AND i.width >= 600 AND i.height >=300 ORDER BY i.id DESC
+			WHERE (n.tid = 137 AND n.status = 1) AND i.city_id = $city_id AND i.width >= 600 AND i.height >=300 ORDER BY i.id DESC
 		) AS t
 		WHERE t.rn < 3 LIMIT 5 )
 		UNION
@@ -33,7 +33,7 @@ BEGIN
 		FROM (
 			SELECT i.nid, i.name, i.path, n.title, @rn := IF( @nid = i.nid, @rn := @rn + 1, 1 ) AS rn, @nid := i.nid
 			FROM images AS i JOIN nodes AS n ON i.nid = n.id 
-			WHERE (n.tid != 139 AND n.status = 1) AND i.city_id = $city_id AND i.width >= 600 AND i.height >=300 ORDER BY i.id DESC
+			WHERE (n.tid != 137 AND n.status = 1) AND i.city_id = $city_id AND i.width >= 600 AND i.height >=300 ORDER BY i.id DESC
 		) AS t
 		WHERE t.rn < 3 LIMIT 5 );
 	END IF;
