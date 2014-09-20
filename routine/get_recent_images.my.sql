56dfeffd32a2a5453da0c19dfed5e223ec9ec48a
Alex Tian <alex.tian@geekpush.com>
2014-09-20 05:41:59 +0000

    enable austinbbs

--- routine/get_recent_images.my.sql
+++ routine/get_recent_images.my.sql
@@ -36,6 +36,22 @@ BEGIN
 			WHERE (n.tid != 137 AND n.status = 1) AND i.city_id = $city_id AND i.width >= 600 AND i.height >=300 ORDER BY i.id DESC
 		) AS t
 		WHERE t.rn < 3 LIMIT 5 );
+	ELSE
+		( SELECT nid, name, path, title 
+		FROM (
+			SELECT i.nid, i.name, i.path, n.title, @rn := IF( @nid = i.nid, @rn := @rn + 1, 1 ) AS rn, @nid := i.nid
+			FROM images AS i JOIN nodes AS n ON i.nid = n.id 
+			WHERE (n.tid = 170 AND n.status = 1) AND i.city_id = $city_id AND i.width >= 600 AND i.height >=300 ORDER BY i.id DESC
+		) AS t
+		WHERE t.rn < 3 LIMIT 5 )
+		UNION
+		( SELECT nid, name, path, title 
+		FROM (
+			SELECT i.nid, i.name, i.path, n.title, @rn := IF( @nid = i.nid, @rn := @rn + 1, 1 ) AS rn, @nid := i.nid
+			FROM images AS i JOIN nodes AS n ON i.nid = n.id 
+			WHERE (n.tid != 170 AND n.status = 1) AND i.city_id = $city_id AND i.width >= 600 AND i.height >=300 ORDER BY i.id DESC
+		) AS t
+		WHERE t.rn < 3 LIMIT 5 );
 	END IF;
 END ;;
 DELIMITER ;
