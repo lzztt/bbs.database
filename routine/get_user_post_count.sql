4282e869d52d973d751eb23350f745a3d8f1a5a5
Long <15257546+lzztt@users.noreply.github.com>
2019-04-29 22:34:19 -0700

    also count private messages for post count

--- routine/get_user_post_count.sql
+++ routine/get_user_post_count.sql
@@ -5,6 +5,7 @@ CREATE DEFINER=web@localhost PROCEDURE get_user_post_count(IN $uid INT)
     COMMENT 'get recent post count for a given user'  
 BEGIN
     SELECT (SELECT count(*) FROM nodes WHERE uid = $uid) +
-           (SELECT count(*) FROM comments WHERE uid = $uid) AS c;
+           (SELECT count(*) FROM comments WHERE uid = $uid) +
+           (SELECT count(*) FROM priv_msgs WHERE from_uid = $uid) AS c;
 END ;;
 DELIMITER ;
