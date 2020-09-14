a60e02e92ab1e7440e2bf71f943ff6db7cf60466
Long <15257546+lzztt@users.noreply.github.com>
2020-09-13 21:42:04 -0700

    remove session

--- routine/delete_user.sql
+++ routine/delete_user.sql
@@ -6,7 +6,6 @@ CREATE DEFINER=web@localhost PROCEDURE delete_user(IN $id INT)
 BEGIN
     UPDATE users SET status = 0 WHERE id = $id;
     UPDATE nodes SET status = 0 WHERE uid = $id;
-    DELETE FROM sessions WHERE uid = $id;
     set @now = UNIX_TIMESTAMP();
     INSERT INTO spammers (email, ip, time) SELECT email, last_access_ip, @now FROM users WHERE id = $id;
 
