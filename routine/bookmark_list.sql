c1728b565affcaec010934ca501fac0594349bf6
Long <15257546+lzztt@users.noreply.github.com>
2020-10-28 23:22:07 -0700

    update bookmark list

--- routine/bookmark_list.sql
+++ routine/bookmark_list.sql
@@ -4,6 +4,13 @@ DELIMITER ;;
 CREATE DEFINER=web@localhost PROCEDURE bookmark_list(IN $uid INT, IN  $limit INT, IN $offset INT)
     COMMENT 'list bookmarks of a user'
 BEGIN
-    SELECT n.id, n.title FROM bookmarks AS b JOIN nodes AS n ON b.nid = n.id WHERE b.uid=$uid ORDER BY b.create_time DESC LIMIT $limit OFFSET $offset;
+    SELECT n.id, n.title, n.uid, u.username, n.last_comment_time
+    FROM bookmarks AS b
+        JOIN nodes AS n ON b.nid = n.id
+        JOIN users AS u ON n.uid = u.id
+    WHERE b.uid=$uid
+    ORDER BY b.create_time DESC
+    LIMIT $limit
+    OFFSET $offset;
 END ;;
 DELIMITER ;
