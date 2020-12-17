e2d85ecf9fa18562c4a4d4cf49420fc221030f56
Long <15257546+lzztt@users.noreply.github.com>
2020-12-16 19:15:26 -0800

    replace slow join with fast query using index

--- routine/get_pm_list_2.sql
+++ routine/get_pm_list_2.sql
@@ -4,27 +4,33 @@ DELIMITER ;;
 CREATE DEFINER=web@localhost PROCEDURE get_pm_list_inbox_2(IN $uid INT, IN $limit INT, IN $offset INT)
     COMMENT 'get recent private messages in inbox for a given user'
 BEGIN
-    SELECT g.msg_id, m.body, g.from_uid AS uid,
-        (SELECT username FROM users WHERE id = g.from_uid) AS user,
-        g.pm_time AS time, g.has_new AS is_new
-    FROM priv_msgs AS m
-        JOIN (SELECT msg_id, MAX(time) AS pm_time, from_uid, (SUM(to_status) > COUNT(to_status)) AS has_new FROM priv_msgs WHERE to_uid = $uid AND to_status > 0 GROUP BY msg_id ORDER BY pm_time DESC LIMIT $limit OFFSET $offset) AS g
-        ON m.id = g.msg_id
-    ORDER BY g.pm_time DESC;
+    SELECT msg_id, MAX(time) AS time, from_uid AS uid, (MAX(to_status) > 1) AS is_new,
+        (SELECT username FROM users WHERE id = pm.from_uid) AS user,
+        (SELECT body FROM priv_msgs WHERE id = pm.msg_id) AS body
+    FROM priv_msgs AS pm
+    WHERE to_uid = $uid AND to_status > 0
+    GROUP BY msg_id
+    ORDER BY time DESC
+    LIMIT $limit
+    OFFSET $offset;
 END ;;
 DELIMITER ;
+
+
 DROP PROCEDURE IF EXISTS get_pm_list_sent_2;
 
 DELIMITER ;;
 CREATE DEFINER=web@localhost PROCEDURE get_pm_list_sent_2(IN $uid INT, IN $limit INT, IN $offset INT)
     COMMENT 'get recent private messages in sent box for a given user'
 BEGIN
-    SELECT g.msg_id, m.body, g.to_uid AS uid,
-        (SELECT username FROM users WHERE id = g.to_uid) AS user,
-        g.pm_time AS time
-    FROM priv_msgs AS m
-        JOIN (SELECT msg_id, MAX(time) AS pm_time, to_uid FROM priv_msgs WHERE from_uid = $uid AND from_status > 0 GROUP BY msg_id ORDER BY pm_time DESC LIMIT $limit OFFSET $offset) AS g
-        ON m.id = g.msg_id
-    ORDER BY g.pm_time DESC;
+    SELECT msg_id, MAX(time) AS time, to_uid AS uid,
+        (SELECT username FROM users WHERE id = pm.to_uid) AS user,
+        (SELECT body FROM priv_msgs WHERE id = pm.msg_id) AS body
+    FROM priv_msgs AS pm
+    WHERE from_uid = $uid AND from_status > 0
+    GROUP BY msg_id
+    ORDER BY time DESC
+    LIMIT $limit
+    OFFSET $offset;
 END ;;
 DELIMITER ;
