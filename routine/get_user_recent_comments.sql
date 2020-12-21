5f35099a2a7f44e30af14bbea083b847b9c44a9f
Long <15257546+lzztt@users.noreply.github.com>
2020-12-20 19:43:26 -0800

    exclude node from comment list

--- routine/get_user_recent_comments.sql
+++ routine/get_user_recent_comments.sql
@@ -4,7 +4,7 @@ DELIMITER ;;
 CREATE DEFINER=web@localhost PROCEDURE get_user_recent_comments(IN $tids VARCHAR(16383), IN $uid INT, IN $limit INT)
     COMMENT 'get recent comments for a given user'
 BEGIN
-    SET @sql = CONCAT('SELECT c.nid, n.title, MAX(c.create_time) AS create_time FROM comments AS c JOIN nodes AS n ON c.nid = n.id WHERE c.uid = ', $uid, ' AND c.tid IN (', $tids, ') AND n.status = 1 GROUP BY c.nid ORDER BY create_time DESC LIMIT ', $limit);
+    SET @sql = CONCAT('SELECT c.nid, n.title, MAX(c.create_time) AS create_time FROM comments AS c JOIN nodes AS n ON c.nid = n.id WHERE c.uid = ', $uid, ' AND c.tid IN (', $tids, ') AND n.status = 1 AND c.create_time != n.create_time GROUP BY c.nid ORDER BY create_time DESC LIMIT ', $limit);
 	PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
