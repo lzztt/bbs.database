8d305d8f26f3cebfe1f4a3856722c010c32a5dc5
Long <15257546+lzztt@users.noreply.github.com>
2020-12-23 01:58:20 -0800

    update insert trigger for users table

--- trigger/before_insert_user.sql
+++ trigger/before_insert_user.sql
@@ -7,22 +7,17 @@ BEGIN
 
     IF NEW.status = 0 THEN
         INSERT INTO spammers (email, ip, time) VALUES (NEW.email, NEW.last_access_ip, NEW.create_time);
-        SIGNAL SQLSTATE '02101' SET MESSAGE_TEXT = 'spammer email detected';
+        SIGNAL SQLSTATE '02101' SET MESSAGE_TEXT = '此邮箱没有通过系统的安全检测。';
     END IF;
 
     SELECT COUNT(*) INTO $count FROM spammers WHERE email = NEW.email;
     IF $count > 0 THEN
-        SIGNAL SQLSTATE '02101' SET MESSAGE_TEXT = 'spammer email detected';
-    END IF;
-
-    SELECT COUNT(*) INTO $count FROM users WHERE username = NEW.username;
-    IF $count > 0 THEN
-        SIGNAL SQLSTATE '02102' SET MESSAGE_TEXT = 'username already exists';
+        SIGNAL SQLSTATE '02101' SET MESSAGE_TEXT = '此邮箱没有通过系统的安全检测。';
     END IF;
 
     SELECT COUNT(*) INTO $count FROM users WHERE email = NEW.email;
     IF $count > 0 THEN
-        SIGNAL SQLSTATE '02103' SET MESSAGE_TEXT = 'email already exists';
+        SIGNAL SQLSTATE '02103' SET MESSAGE_TEXT = '此邮箱已被其他用户使用。';
     END IF;
 END ;;
 DELIMITER ;
