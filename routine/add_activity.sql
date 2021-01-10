bc70c70a7beeb775528d6a05e68390cfc28d3ab2
Long <15257546+lzztt@users.noreply.github.com>
2021-01-10 14:59:58 -0800

    clean up activities

--- routine/add_activity.sql
+++
@@ -1,9 +0,0 @@
-DROP PROCEDURE IF EXISTS add_activity;
-
-DELIMITER ;;
-CREATE DEFINER=web@localhost PROCEDURE add_activity(IN $nid INT, IN $begin INT, IN $end INT)
-    COMMENT 'add an activity'
-BEGIN
-    REPLACE INTO activities (nid,start_time,end_time) VALUES ($nid, $begin, $end);
-END ;;
-DELIMITER ;
